aaca24a8c199414920fbcb587e4e56af4e038d1e
Zhijun Wang <zhw106@ucsd.edu>
2025-06-08 22:14:07 -0700

    Update run.bash

--- run.bash
+++ run.bash
@@ -4,3 +4,4 @@ chmod +x detector_input_jsonal_helper.py
 python detectoreval.py 
 python merge_json.py prompt.json responses.json paired.json
 python llm_judged_ability_test.py paired.json > llm_judge_result.txt
+python analysis_qualities.py 
